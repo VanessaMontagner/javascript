52b47347f8f7c9f3622ae50cf43a5c87d9728b22
VanessaMontagner <57117106+VanessaMontagner@users.noreply.github.com>
2019-10-28 22:27:45 -0300

    Adicionei 3 pastas Aula 1

---
+++ funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Função Simples exemplo de uma função Simples
+ * @author Vanessa
+ */
+
+ function helloWorld(){
+    let nome
+    nome = prompt ('Qual é seu nome ?')
+    alert('Hello ' + nome)
+ }
---
+++ vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * Exemplo de vinculação de um documento JS no Html
+ * @author Vanessa
+ */
+
+ document.write('<h2>'+ 'Hello World' +' </h2>')
