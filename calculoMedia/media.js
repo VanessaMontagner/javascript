9ff165d45fc64c31aa6264681603a37133d9d380
VanessaMontagner <57117106+VanessaMontagner@users.noreply.github.com>
2019-11-06 19:05:44 -0300

    Calculo media com aprovação

--- calculoMedia/media.js
+++ calculoMedia/media.js
@@ -7,9 +7,17 @@
 
 let nota1, nota2, media
 
-function calcular(){
-    nota1 = Number (frmMedia.textNota1.value.replace(",","."));
-    nota2 = Number (frmMedia.textNota2.value.replace(",","."));
-    media = (nota1 + nota2) /2
-    frmMedia.textMedia.value=media.toFixed(1)
+function calcular() {
+    nota1 = Number(frmMedia.textNota1.value.replace(",", "."));
+    nota2 = Number(frmMedia.textNota2.value.replace(",", "."));
+    media = (nota1 + nota2) / 2
+    frmMedia.textMedia.value = media.toFixed(2)
+    if (media < 5) {
+        frmMedia.txtStatus.value = 'Reprovado'
+    } else {
+        frmMedia.txtStatus.value = 'Aprovado'
+    }
+
+
 }
+
