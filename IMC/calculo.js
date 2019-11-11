8ca4fec9a12dc3c10ccc992f8d99797463ee4af3
VanessaMontagner <57117106+VanessaMontagner@users.noreply.github.com>
2019-11-10 22:03:47 -0300

    Imc Completo

--- IMC/calculo.js
+++ IMC/calculo.js
@@ -3,14 +3,30 @@
  * @author Vanessa
  */
 
- let altura, peso, calculo1, calculoCompleto
+ let altura, peso, calculo1, calculo2
 
  function calcular() {
      altura = Number (imc.altura.value.replace(",","."))
      peso = Number (imc.peso.value.replace(",","."))
      calculo1 =  peso / (altura*altura)
+
      
      imc.calculoImc.value=calculo1.toFixed(2)
 
-     
+     if (calculo1 <17) {
+        document.getElementById("status").src='imc1.png'
+    } else if (calculo1 >17 && calculo1 <18.49){
+        document.getElementById("status").src='imc2.png'
+    } else if (calculo1 >18.5 && calculo1 <24,99){
+        document.getElementById("status").src='imc3.png'
+    } else if (calculo1 >25 && calculo1 <29.99){
+        document.getElementById("status").src='imc4.png'
+    } else if (calculo1 >30 && calculo1 <34.99){
+        document.getElementById("status").src='imc5.png'
+    } else if (calculo1 >35 && calculo1 <39.99){
+        document.getElementById("status").src='imc6.png'
+    } else if (calculo1 >40){
+        document.getElementById("status").src='imc6.png'
+    } 
+
  }
