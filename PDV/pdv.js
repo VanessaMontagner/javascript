c24ad2d5d057aa47bfb22eb34c16d3ff1e9b9baa
VanessaMontagner <57117106+VanessaMontagner@users.noreply.github.com>
2019-11-18 19:52:42 -0300

    PDV

--- PDV/pdv.js
+++ PDV/pdv.js
@@ -10,10 +10,10 @@ function calcular() {
     desconto = Number (pdv.porcentagem.value.replace(',','.'))
     total1 =  (total * desconto ) / 100
     total2 = total - total1
-    pdv.total.value=total2.toFixed(2)
+    pdv.total.value= 'R$ ' + total2.toFixed(2)
     pago = Number (pdv.valorPago.value.replace(',','.'))
     troco = pago - total2
-    pdv.troco.value=troco.toFixed(2)
+    pdv.troco.value= 'R$ ' + troco.toFixed(2)
     
 
     
